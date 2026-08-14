4342b43f7ab679865613632d7649fc482ca4ab0c
Danush A <123644307+danush-20@users.noreply.github.com>
2026-08-14 10:39:59 +0530

    08-14-2026__Easy__0001-two-sum

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i in range(len(nums)):
+            if nums[i] in d.keys():
+                return [d[nums[i]],i]
+            d[target-nums[i]]=i
+        
