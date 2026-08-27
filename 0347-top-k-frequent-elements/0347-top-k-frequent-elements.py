143e128456206b5603122de2da4d1af1e5d2f269
Danush A <123644307+danush-20@users.noreply.github.com>
2026-08-27 21:14:08 +0530

    08-27-2026__Medium__0347-top-k-frequent-elements

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,23 @@
+class Solution:
+    def topKFrequent(self, nums, k):
+        d = {}
+        for i in nums:
+            if i not in d.keys():
+                d[i] = 1
+            else:
+                d[i] = d.get(i) + 1
+
+        l = []
+        for i in range(k):
+            v = self.max(d)
+            l.append(v)
+            del d[v]
+        return l
+
+    def max(self,d):
+        value, key = 0, 0
+        for i, j in d.items():
+            if value < j:
+                value = j
+                key = i
+        return key
