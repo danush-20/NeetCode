624ff3cd813ae5a0159a06340f46c55795fe9ff0
Danush A <123644307+danush-20@users.noreply.github.com>
2026-09-17 10:53:40 +0530

    Time: 0 ms (100%), Space: 19.2 MB (63.19%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.py
@@ -0,0 +1,27 @@
+class Solution:
+    def sortColors(self, nums: list[int]) -> None:
+        low , mid = 0,0
+        high = len(nums)-1
+
+        while (mid <= high):
+            if (nums[mid] == 0):
+                temp = nums[low]
+                nums[low] = nums[mid]
+                nums[mid] = temp
+                low+=1
+                mid+=1
+            elif (nums[mid] == 1):
+                mid+=1
+            else:
+                temp = nums[mid]
+                nums[mid] = nums[high]
+                nums[high] = temp
+                high-=1
+
+            
+
+        
+        
+
+            
+        
